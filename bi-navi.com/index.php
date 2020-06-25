e89360990b391e1a7d5ce4c69a2e6315211b8e6c
verandacat <verandacats@yahoo.co.jp>
2020-06-25 19:16:29 +0900

    bi-navi 最新

---
+++ bi-navi.com/index.php
@@ -0,0 +1,19 @@
+<?php
+$_SESSION['switch'] = $_GET['switch'];
+if ($_GET['pat'] == "a") {
+    include_once('./index_a.php');
+} elseif ($_GET['pat'] == "b") {
+    include_once('./index_b.php');
+} elseif ($_GET['pat'] == "c") {
+    include_once('./index_c.php');
+} elseif ($_GET['pat'] == "d") {
+    include_once('./index_d.php');
+} elseif ($_GET['pat'] == "e") {
+    include_once('./index_e.php');
+} elseif ($_GET['pat'] == "f") {
+    include_once('./index_f.php');
+} elseif ($_GET['pat'] == "g") {
+    include_once('./index_g.php');
+} else {
+    include_once('./index_a.php');
+}
